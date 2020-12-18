e6cf256fab780174dfb8ec8d0ca27fe6f027f9a7
RVBelyaevsky <Rooman83@mail.ru>
2020-12-18 16:15:07 +0800

    first commit

---
+++ js/sctipt.js
@@ -0,0 +1,23 @@
+"use strict";
+
+
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+
+const answer1 = prompt("Один из последних просмотренных  фильмов?", '');
+const answer2 = prompt("На сколько оцените его?", '');
+const answer3 = prompt("Один из последних просмотренных  фильмов?", '');
+const answer4 = prompt("На сколько оцените его?", '');
+
+personalMovieDB.movies[answer1] = answer2;
+personalMovieDB.movies[answer3] = answer4;
+
+console.log(personalMovieDB);
